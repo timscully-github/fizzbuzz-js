a9a2efcf7956321b16a39c72f6bf40edb042b5de
Tim Scully <hello@timscully.co.uk>
2020-05-12 17:27:18 +0100

    added Buzz for divisible by 5

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,9 @@ var n = 1;
 
 function fizzBuzz() {
     n++;
-    if (n % 3 === 0){
+    if (n % 5 === 0){
+      output.push("Buzz");
+    } else if (n % 3 === 0){
       output.push("Fizz");
     } else {
       output.push(n);
