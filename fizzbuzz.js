e8f29d5a3f8f4c3e62917ec3db780721eaeabd49
Tim Scully <hello@timscully.co.uk>
2020-05-12 17:33:54 +0100

    changed count so that it increments from 1

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,13 +2,15 @@ var output = [];
 var n = 1;
 
 function fizzBuzz() {
-    n++;
-    if (n % 5 === 0){
+    if (n % 15 === 0){
+      output.push("FizzBuzz");
+    } else if (n % 5 === 0){
       output.push("Buzz");
     } else if (n % 3 === 0){
       output.push("Fizz");
     } else {
       output.push(n);
     }
+    n++;
     console.log(output);
 }
