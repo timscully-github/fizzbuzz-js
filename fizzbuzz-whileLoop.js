4a4fbe64972a3619479c925c7accdc74353feb5e
Tim Scully <hello@timscully.co.uk>
2020-05-13 11:46:05 +0100

    updated to call function

--- fizzbuzz-whileLoop.js
+++ fizzbuzz-whileLoop.js
@@ -4,16 +4,20 @@ var n = 1;
 function fizzBuzz() {
 
     while(n <= 100){
-      if (n % 15 === 0){
-        output.push("FizzBuzz");
-      } else if (n % 5 === 0){
-        output.push("Buzz");
-      } else if (n % 3 === 0){
-        output.push("Fizz");
-      } else {
-        output.push(n);
-      }
-      n++;
+
+        if (n % 15 === 0){
+          output.push("FizzBuzz");
+        } else if (n % 5 === 0){
+          output.push("Buzz");
+        } else if (n % 3 === 0){
+          output.push("Fizz");
+        } else {
+          output.push(n);
+        }
+        n++;
+
     }
     console.log(output);
 }
+
+fizzBuzz();
